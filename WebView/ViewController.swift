c223e00371a8c5258a64c0621c4931309d060c5f
alanhuerta <alan_huerta_esc@outlook.com>
2023-06-30 11:52:42 -0600

    Allow unverified ssl certificates in WKWebview

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -4,16 +4,34 @@
 //
 //  Created by Alan Antonio Huerta Cortes on 30/06/23.
 //
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: view.bounds, configuration: webConfiguration)
+        webView.navigationDelegate = self
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 [D-iOS; ClaroConnect/2.0.6]"
+        view.addSubview(webView)
+
+        let url = URL(string: "https://front.claroconnect.com/iam/c")!
+        let request = URLRequest(url: url)
+
+        webView.load(request)
     }
 
-
+    //Override webview
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        guard let serverTrust = challenge.protectionSpace.serverTrust else { return completionHandler(.useCredential, nil) }
+        let exceptions = SecTrustCopyExceptions(serverTrust)
+        SecTrustSetExceptions(serverTrust, exceptions)
+        completionHandler(.useCredential, URLCredential(trust: serverTrust))
+    }
 }
+
 
